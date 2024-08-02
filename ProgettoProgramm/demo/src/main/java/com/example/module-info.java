1b583a7277a810e85c6d26a784abcc470f8e22c7
Aivon99 <109174292+Aivon99@users.noreply.github.com>
2024-08-02 03:53:19 +0200

    Delete ProgettoProgramm/demo/src/main/java/com/example/module-info.java
    Adesso dovrebbe funzionare ed essere aggiornato al 01/08/2024. Problemi con NumberSumExercise, domani ci guardo

--- ProgettoProgramm/demo/src/main/java/com/example/module-info.java
+++
@@ -1,7 +0,0 @@
-module progetto.programmazione {
-    requires transitive javafx.controls;
-    requires javafx.fxml;
-
-    opens progetto.programmazione to javafx.fxml;
-    exports progetto.programmazione;
-}
